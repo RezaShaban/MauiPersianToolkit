01a8d0c6a6a9b6d9ac1572014640b301aa188881
Reza <reza.shaban.2011@gmail.com>
2023-06-09 17:26:36 +0330

    remove entry & editor bottom border release version 1.0.1.0

--- PersianUIControlsMaui/AppBuilderExtensions.cs
+++ PersianUIControlsMaui/AppBuilderExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Maui;
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#endif
 
 namespace PersianUIControlsMaui;
 
@@ -13,7 +16,24 @@ public static class AppBuilderExtensions
                 //fonts.AddEmbeddedResourceFont(typeof(AppBuilderExtensions).Assembly, "materialdesignicons-webfont.ttf", "MDI");
                 fonts.AddEmbeddedResourceFont(typeof(AppBuilderExtensions).Assembly, "IranianSans.ttf", "IranianSans");
             });
-
+        Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("CustomEditor", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.Bottom = 0;
+            Android.Graphics.Drawables.GradientDrawable gd = new Android.Graphics.Drawables.GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+            handler.PlatformView.SetBackgroundDrawable(gd);
+#endif
+        });
+        // Remove Entry control underline
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("CustomEntry", (handler, view) =>
+        {
+#if IOS || MACCATALYST
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif ANDROID
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#endif
+        });
         return builder;
     }
 }
